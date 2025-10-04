5b0247463a2243578ad867cc3f9a6c54c060792f
luke89joseph <luke87joseph@gmail.com>
2025-10-04 00:00:36 -0400

    End of Day Oct 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+    h1 {
+        font-family: Montserrat;
+    }
+
+    :global(body) {
+        background-color: pink;
+    }
+
+</style>
+
 <h1>Bienvenue à "Étoile"!</h1>
 <p>Visitez <a href="https://hackclub.com">Hack Club</a>!</p>
